ac3038165cab59c0e20c87cfdf4a2a5bc208163a
aetherz <aetherz13@gmail.com>
2021-03-09 19:18:39 +0800

    create 4 func

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from . import serializers
+from rest_framework import status
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
+
+
+
     def get(self,request,format=None):
         """Return a list of APIView features"""
 
@@ -14,5 +21,26 @@ class HelloApiView(APIView):
         'it mapped mannually to urls'
         ]
         return Response({'message':'Hello!','an_apiview':an_apiview})
-        
+
+    def post(self,request):
+        """create a hello message with our name"""
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        """handles updating an object"""
+        return Response({'method':'put'})
+
+    def patch(self,request,pk=None):
+        """Patch request , only update fields provided in the request"""
+        return Response({'method':'patch'})
+
+    def delete(self,request,pk=None):
+        """delete an object"""
+        return Response({'method':'delete'})
 # Create your views here.
